f7d14a8f512426ef754637d7703ed0cb7cbc51e6
Lucas Portilho <lucasportilho18@gmail.com>
2023-03-03 20:07:28 -0300

    feat. req 11 incompleto

---
+++ challenges/desafio11.sql
@@ -0,0 +1,9 @@
+SELECT alb.nome_album AS 'album', COUNT(c.id) AS 'favoritadas'
+FROM SpotifyClone.album AS alb
+INNER JOIN SpotifyClone.cancao AS c
+ON alb.id = c.album_id;
+INNER JOIN SpotifyClone.favoritas AS f
+ON c.id = f.cancao_id
+GROUP BY alb.nome_album
+ORDER BY favoritadas DESC, album ASC
+LIMIT 3;
